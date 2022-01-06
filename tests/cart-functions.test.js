6240303aa6b6018af3fa153a0ed80cbc6c5a618d
Renato Dodani <renato.dodani1@gmail.com>
2022-01-05 21:56:23 -0500

    Added tests for the functions I added

--- tests/cart-functions.test.js
+++ tests/cart-functions.test.js
@@ -1,19 +1,37 @@
+const cartFunctions = require('../src/js/cart-functions');
+
+
 describe("calculateChange", () => {
-  test.todo("add calculateChange tests here");
+  test("calculateChange", () => {
+    expect(cartFunctions.calculateChange(5, 6)).toEqual(1);
+  });
+  test("should return .73 when given 12.3 and 13.03", () => {
+    expect(cartFunctions.calculateChange(12.3, 13.03)).toEqual(0.73)
+  });
+
 });
 
-describe("isSufficientPayment", () => {
-  test.todo("add isSufficientPayment tests here");
-});
+// describe("isSufficientPayment", () => {
+//   test("should return true when given 5 and 6", () => {
+//     expect(cartFunctions.isSufficientPayment(5, 6).toEqual(true)
+  
+  
 
+//   });
+// });
 describe("calculateTotal", () => {
-  test.todo("add calculateTotal tests here");
+  test("should return a total of 4.99 with one item of costing 4.99", () => {
+    const mockItemsArray = [{name: 'Taco', price: 4.99}]
+    expect(cartFunctions.calculateTotal(mockItemsArray)).toEqual(4.99);
+  });
+
+  
 });
 
-describe("addItem", () => {
-  test.todo("add addItem tests here");
-});
+// describe("addItem", () => {
+//   test("add addItem tests here");
+// });
 
-describe("removeItem", () => {
-  test.todo("add removeItem tests here");
-});
+// describe("removeItem", () => {
+//   test("add removeItem tests here");
+// });
